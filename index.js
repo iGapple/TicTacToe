fd575952a2bcda75cd147bd5d8d794e8282c6c5d
iGapple <g4pple35@gmail.com>
2022-09-01 23:18:12 +0500

    nice clean code

---
+++ index.js
@@ -0,0 +1,14 @@
+//start of declarations
+var usr = "circle";
+var cell1 = document.getElementById("cell1")
+var cell2 = document.getElementById("cell2")
+var cell3 = document.getElementById("cell3")
+var cell4 = document.getElementById("cell4")
+var cell5 = document.getElementById("cell5")
+var cell6 = document.getElementById("cell6")
+var cell7 = document.getElementById("cell7")
+var cell8 = document.getElementById("cell8")
+var cell9 = document.getElementById("cell9")
+let user = document.getElementById("TIC");
+let cells = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
+//end of declarations
